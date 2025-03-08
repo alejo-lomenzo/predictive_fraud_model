fe046786b1ce4f4eed679c2500e46b66bce79870
alejo lo menzo <alejolomenzo@gmail.com>
2025-03-08 17:04:00 -0300

    Initialize project using Cookiecutter Data Science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository contains the code for a model to detect fraud.',
+    author='Lo Menzo Alejo',
+    license='MIT',
+)
